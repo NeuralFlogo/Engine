b962261dad39a1fc165e5c1c39b28f90067a176e
juancarss <juancarss2002@gmail.com>
2023-03-29 09:10:24 +0100

    Numeric Processor implemented

--- preprocesing/NumericProcessor.py
+++ preprocesing/NumericProcessor.py
@@ -1,8 +1,18 @@
+import torch
+import torch.nn.functional as f
 
 
-def standardize():
-    return
+def encode(col):
+    return f.one_hot(col[:, :-1], torch.unique(col[:, :-1]).len())
 
 
-def normalize():
-    return
+def standardize(col):
+    return (col - torch.mean(col)) / torch.std(col)
+
+
+def normalize(col, new_min, new_max):
+    return (col - col.min()) / (col.max() - col.min()) * (new_max - new_min) + new_min
+
+
+def l_normalize(col, p, dim=1):
+    return f.normalize(col, p, dim)
