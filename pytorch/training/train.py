f368121b5e6fac4c9c685f6d17137623584b0289
juancarss <juancarss2002@gmail.com>
2023-04-13 09:17:24 +0100

    Train method completed

--- pytorch/training/train.py
+++ pytorch/training/train.py
@@ -1,62 +1,72 @@
+from datetime import datetime
+
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from pytorch.preprocesing.NumericProcessor import one_hot_encode
-
 
 class Training:
-    def __init__(self, epochs, model, training_loader, validation_loader, loss_function, optimizer):
-        self.epochs = epochs
+    def __init__(self, model, epochs, training_loader, validation_loader, optimizer, loss_function):
         self.model = model
+        self.epochs = epochs
         self.training_loader = training_loader
         self.validation_loader = validation_loader
-        self.loss_function = loss_function
         self.optimizer = optimizer
-        self.writer = SummaryWriter("runs")
+        self.loss_function = loss_function
+        self.timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        self.writer = SummaryWriter("runs/model_{}".format(self.timestamp))
 
     def train(self):
         best_vloss = 1_000_000.
         for epoch in range(self.epochs):
-            print('EPOCH {}:'.format(epoch + 1))
             self.model.train(True)
             avg_loss = self.__train_epoch(epoch)
             self.model.train(False)
             avg_vloss = self.__validate_epoch()
-            print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
-            self.writer.add_scalars('Training vs Validation Loss',
-                                    {'Training': avg_loss, 'Validation': avg_vloss},
-                                    epoch + 1)
-            print('Training vs Validation Loss',
-                  {'Training': avg_loss, 'Validation': avg_vloss},
-                  epoch + 1)
+            self.__log_epoch_losses(epoch, avg_loss, avg_vloss)
             self.writer.flush()
             if avg_vloss < best_vloss:
                 best_vloss = avg_vloss
-                torch.save(self.model.state_dict(), 'models/model_{}'.format(epoch))
+                self.__save_model(epoch)
         self.writer.close()
 
     def __train_epoch(self, epoch):
-
-        running_loss = 0.
-        for i, data in enumerate(self.training_loader):
+        loss = 0.
+        for i, data in enumerate(self.training_loader, start=1):
             inputs, labels = data
             self.optimizer.zero_grad()
-            outputs = self.model(inputs)
-            labels = labels.squeeze(1)
-            loss = self.loss_function(outputs, labels)
+            loss = self.__compute_loss(inputs, labels)
             loss.backward()
+            loss += loss.item()
             self.optimizer.step()
-            running_loss += loss.item()
-            # print('batch {} loss: {}'.format(i + 1, running_loss))
-            tb_x = epoch * len(self.training_loader) + i + 1
-            self.writer.add_scalar('Loss/train', running_loss, tb_x)
-        return running_loss / (i + 1)
+            self.__log_train_loss(self.__training_count(epoch, i), loss)
+        return self.__epoch_average_loss(loss)
+
+    def __log_train_loss(self, training_count, loss):
+        self.writer.add_scalar('Train Loss', loss, training_count)
 
     def __validate_epoch(self):
-        running_vloss = 0.
-        for i, data in enumerate(self.validation_loader):
+        vloss = 0.
+        for data in enumerate(self.validation_loader, start=1):
             inputs, labels = data
-            outputs = self.model(inputs)
-            loss = self.loss_function(outputs, labels)
-            running_vloss += loss.item()
-        return running_vloss / (i + 1)
+            vloss += self.__compute_loss(inputs, labels).item()
+        return self.__epoch_average_loss(vloss)
+
+    def __log_epoch_losses(self, epoch, avg_loss, avg_vloss):
+        self.writer.add_scalars('Training - Validation Loss',
+                                {'Training': avg_loss, 'Validation': avg_vloss},
+                                epoch + 1)
+
+    def __save_model(self, epoch):
+        torch.save(self.model.state_dict(), 'model_{}_{}'.format(self.timestamp, epoch))
+
+    def __compute_loss(self, inputs, labels):
+        return self.loss_function(self.__evaluate(inputs), labels)
+
+    def __evaluate(self, inputs):
+        return self.model(inputs)
+
+    def __training_count(self, epoch, i):
+        return epoch * len(self.training_loader) + i
+
+    def __epoch_average_loss(self, loss):
+        return loss / self.epochs
