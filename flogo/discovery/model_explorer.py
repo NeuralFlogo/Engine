969494eccb9dd5738b9414d2922d9058dbd1f257
juancarss <juancarss2002@gmail.com>
2023-05-12 13:01:48 +0100

    model discovery done

--- flogo/discovery/model_explorer.py
+++ flogo/discovery/model_explorer.py
@@ -1,13 +1,18 @@
 class ModelExplorer:
-    def __init__(self, training_tasks, test_task):
+    def __init__(self, models, training_dataset, validation_dataset, training_tasks, test_task):
+        self.models = models
+        self.training_dataset = training_dataset
+        self.validation_dataset = validation_dataset
         self.training_tasks = training_tasks
         self.test_task = test_task
 
-    def explore(self):
-        return self.__select_best_model([self.__compute_accuracy(training) for training in self.training_tasks])
+    def explore(self, epochs):
+        return self.__select_best_model([self.__compute_accuracy(index, epochs) for index in range(self.training_tasks)])
 
     def __select_best_model(self, accuracies):
-        return self.training_tasks[accuracies.index(max(accuracies))].architecture, max(accuracies)
+        return self.models[accuracies.index(max(accuracies))], max(accuracies)
 
-    def __compute_accuracy(self, training):
-        return self.test_task.execute(training.execute())
+    def __compute_accuracy(self, index, epochs):
+        return self.test_task.execute(
+            self.training_tasks[index].execute(epochs, self.models[index], self.training_dataset, self.validation_dataset)
+        )
