1a1fd2aae7c8b9508758b077d5eaf04b4f277e3b
juancarss <juancarss2002@gmail.com>
2023-05-12 12:34:19 +0100

    monitor fixed

--- flogo/discovery/regularization/monitors/growth_monitor.py
+++ flogo/discovery/regularization/monitors/growth_monitor.py
@@ -4,12 +4,16 @@ from math import inf
 class GrowthMonitor:
     def __init__(self, patience: int, improvement_threshold: float, metric="Accuracy"):
         self.improvement_threshold = improvement_threshold
-        self.history = self.__init_history(patience, metric)
+        self.metric = metric
+        self.history = self.__init_history(patience)
 
-    def __init_history(self, patience, metric):
-        return [0] * (patience + 1) if metric == "Accuracy" else [inf] * (patience + 1)
+    def __init_history(self, patience):
+        return [0] * (patience + 1) if self.metric == "Accuracy" else [inf] * (patience + 1)
 
     def supervise(self, value) -> bool:
         self.history.append(value)
-        return max(self.history) - self.history.pop(0) < self.improvement_threshold
+        return abs(self.__find_edge_value() - self.history.pop(0)) < self.improvement_threshold
+
+    def __find_edge_value(self):
+        return max(self.history) if self.metric == "Accuracy" else min(self.history)
 
