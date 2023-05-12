1c849789dd54b75c771b3ec8010a7c927a0c284d
juancarss <juancarss2002@gmail.com>
2023-05-12 12:23:10 +0100

    Bug fixed

--- flogo/discovery/regularization/monitors/growth_monitor.py
+++ flogo/discovery/regularization/monitors/growth_monitor.py
@@ -1,10 +1,13 @@
-class GrowthMonitor:
-    def __init__(self, patience: int, improvement_threshold: float):
-        self.improvement_threshold = improvement_threshold
-        self.history = self.__init_history(patience)
+from math import inf
 
-    def __init_history(self, patience):
-        return [0] * (patience + 1)
+
+class GrowthMonitor:
+    def __init__(self, patience: int, improvement_threshold: float, metric="Accuracy"):
+        self.improvement_threshold = improvement_threshold
+        self.history = self.__init_history(patience, metric)
+
+    def __init_history(self, patience, metric):
+        return [0] * (patience + 1) if metric == "Accuracy" else [inf] * (patience + 1)
 
     def supervise(self, value) -> bool:
         self.history.append(value)
