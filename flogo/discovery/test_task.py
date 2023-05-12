25432ccfaa29cf78ff8ca71e087469b128dc3cce
juancarss <juancarss2002@gmail.com>
2023-05-12 11:48:18 +0100

    Test task improved

--- flogo/discovery/test_task.py
+++ flogo/discovery/test_task.py
@@ -2,9 +2,11 @@ from pytorch.discovery.tester import PytorchTester
 
 
 class TestTask:
-    def __init__(self, dataset, task: PytorchTester.__class__):
-        self.dataset = dataset
-        self.task = task
+    def __init__(self, dataset, measurer, task: PytorchTester.__class__):
+        self.task = self.__init_task(task, dataset, measurer)
+
+    def __init_task(self, task, dataset, measurer):
+        return task(dataset, measurer)
 
     def test(self, model):
-        return self.task(self.dataset).execute(model)
+        return self.task.execute(model)
--- pytorch/discovery/measurers/accuracy_measurer.py
+++ pytorch/discovery/measurers/accuracy_measurer.py
@@ -2,8 +2,8 @@ import torch
 
 
 class AccuracyMeasurer:
-    def measure(self, predictions, labels):
-        return torch.sum(torch.eq(self.__predicted_class(predictions), self.__predicted_class(labels)))
+    def measure(self, predictions, expected):
+        return torch.sum(torch.eq(self.__predicted_class(predictions), self.__predicted_class(expected)))
 
     def __predicted_class(self, predictions):
         return torch.argmax(predictions, dim=1)
